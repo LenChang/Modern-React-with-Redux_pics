e707e7ac18a39a33b734061b55d04d32a7355538
Len Chang <lamgo.chang@gmail.com>
2020-04-10 15:35:55 +0800

    Modern React with Redux: Section 9 - 97 to 101: Show image list

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import SearchBar from "./SearchBar";
 import axios from "../api/unsplash";
+import ImageList from "../components/ImageList";
 class App extends React.Component {
   state = { images: [] };
 
@@ -17,7 +18,7 @@ class App extends React.Component {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
-        Found:{this.state.images.length} images
+        <ImageList images={this.state.images} />
       </div>
     );
   }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 class SearchBar extends React.Component {
-  state = { term: "Hi there" };
+  state = { term: "Taiwan" };
 
   // onInputChange(event) {
   //   console.log(event.target.value);
