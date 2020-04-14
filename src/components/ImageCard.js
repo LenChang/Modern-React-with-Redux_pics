c175af8742b42dec3c418b41eb0e76de463d76b7
Len Chang <lamgo.chang@gmail.com>
2020-04-14 14:38:46 +0800

    Modern React with Redux: Section 10 - 108 Dynamic Spans

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -4,23 +4,30 @@ class ImageCard extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = { spans: 0 };
+
     this.imageRef = React.createRef();
+    // console.log(`constructor:${this.imageRef.current}`);
   }
 
   componentDidMount() {
-    // console.log(this.imageRef.current.clientHeight);
-    this.imageRef.current.addEventListener("load", this.setSpans);
+    // console.log(`constructor:${this.imageRef.current}`);
+    // this.imageRef.current.addEventListener("load", this.setSpans);
+    const height = this.imageRef.current.clientHeight;
+    const spans = Math.ceil(height / 10);
+
+    this.setState({ spans });
   }
 
-  setSpans = () => {
-    console.log(this.imageRef.current.clientHeight);
-  };
+  // setSpans = () => {
+  //   console.log(this.imageRef.current.clientHeight);
+  // };
 
   render() {
     const { description, urls } = this.props.image;
 
     return (
-      <div>
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
         <img ref={this.imageRef} alt={description} src={urls.regular}></img>
       </div>
     );
